584ae9769133bbc45de3eedfb2e3e9517c58e00b
Alex Schirokow <aschirokow@gmx.de>
2024-04-06 19:59:08 +0200

    adding second table to staging: staging_locations

--- models/staging/staging_location.sql
+++ models/staging/staging_location.sql
@@ -1,13 +1,14 @@
-WITH temperature_daily AS (
-    SELECT ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'date')::VARCHAR)::date  AS date,
-        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'maxtemp_c')::VARCHAR)::FLOAT AS maxtemp_c,
-        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'mintemp_c')::VARCHAR)::FLOAT AS mintemp_c,
-        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avgtemp_c')::VARCHAR)::FLOAT AS avgtemp_c,
-        (extracted_data -> 'location' -> 'name')::VARCHAR  AS city,
-        (extracted_data -> 'location' -> 'region')::VARCHAR  AS region,
-        (extracted_data -> 'location' -> 'country')::VARCHAR  AS country,
-        ((extracted_data -> 'location' -> 'lat')::VARCHAR)::NUMERIC  AS lat, 
-        ((extracted_data -> 'location' -> 'lon')::VARCHAR)::NUMERIC  AS lon
+WITH locations_data AS (
+    SELECT DISTINCT
+		   --(extracted_data ->> 'q')  AS location,
+           (extracted_data -> 'location' ->> 'name')::VARCHAR(255) AS city
+           ,(extracted_data -> 'location' ->> 'region')::VARCHAR(255) AS region
+           ,(extracted_data -> 'location' ->> 'country')::VARCHAR(255) AS country
+           ,(extracted_data -> 'location' ->> 'lat')::NUMERIC AS lat 
+           ,(extracted_data -> 'location' ->> 'lon')::NUMERIC AS lon
+           ,(extracted_data -> 'location' ->> 'tz_id') AS timezone_id
+--           ,TO_TIMESTAMP((extracted_data -> 'location' ->> 'localtime_epoch')::BIGINT) AS time_epoch
+--           ,(extracted_data -> 'location' ->> 'localtime')::TIMESTAMP AS time_local
     FROM {{source("staging", "weather_raw")}})
 SELECT * 
-FROM temperature_daily
+FROM locations_data
