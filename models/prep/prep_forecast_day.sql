6ca4d5b485fa15c35b169a0566ed8a5d6c3a8f90
Alex Schirokow <aschirokow@gmx.de>
2024-04-07 19:13:48 +0200

    prep removing all semicolons

--- models/prep/prep_forecast_day.sql
+++ models/prep/prep_forecast_day.sql
@@ -14,10 +14,10 @@ add_features AS (
     FROM forecast_day_data
 )
 SELECT *
-FROM add_features;
+FROM add_features
 
 -- Update NULL values for specific columns
-UPDATE forecast_day_data SET moonrise = NULL WHERE moonrise = 'No moonrise';
-UPDATE forecast_day_data SET moonset = NULL WHERE moonset = 'No moonset';
-UPDATE forecast_day_data SET sunrise = NULL WHERE sunrise = 'No sunrise';
-UPDATE forecast_day_data SET sunset = NULL WHERE sunset = 'No sunset';
+UPDATE forecast_day_data SET moonrise = NULL WHERE moonrise = 'No moonrise'
+UPDATE forecast_day_data SET moonset = NULL WHERE moonset = 'No moonset'
+UPDATE forecast_day_data SET sunrise = NULL WHERE sunrise = 'No sunrise'
+UPDATE forecast_day_data SET sunset = NULL WHERE sunset = 'No sunset'
