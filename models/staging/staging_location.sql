83d138cb084b36c2de867dc1d5a19aec4cca91d8
Alex Schirokow <aschirokow@gmx.de>
2024-04-07 18:50:44 +0200

    staging initial commit

--- models/staging/staging_location.sql
+++ models/staging/staging_location.sql
@@ -1,14 +1,14 @@
 WITH locations_data AS (
     SELECT DISTINCT
 		   --(extracted_data ->> 'q')  AS location,
-           (extracted_data -> 'location' ->> 'name')::VARCHAR(255) AS city
-           ,(extracted_data -> 'location' ->> 'region')::VARCHAR(255) AS region
-           ,(extracted_data -> 'location' ->> 'country')::VARCHAR(255) AS country
+           (extracted_data->'location'->>'name')::VARCHAR(255) AS city
+           ,(extracted_data-> 'location'->>'region')::VARCHAR(255) AS region
+           ,(extracted_data -> 'location'->> 'country')::VARCHAR(255) AS country
            ,(extracted_data -> 'location' ->> 'lat')::NUMERIC AS lat 
            ,(extracted_data -> 'location' ->> 'lon')::NUMERIC AS lon
            ,(extracted_data -> 'location' ->> 'tz_id') AS timezone_id
-          ,TO_TIMESTAMP((extracted_data -> 'location' ->> 'localtime_epoch')::BIGINT) AS time_epoch
+        --   ,TO_TIMESTAMP((extracted_data -> 'location' ->> 'localtime_epoch')::BIGINT) AS time_epoch
 --           ,(extracted_data -> 'location' ->> 'localtime')::TIMESTAMP AS time_local
     FROM {{source("staging", "weather_raw")}})
 SELECT * 
-FROM locations_data
+FROM locations_data;
